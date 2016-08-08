2a4c5bad5a766ef6741ab155ccca8755e43d6cff
FortuneDream <812568684@qq.com>
2016-08-08 22:07:38 +0800

    注释修改

--- app/src/main/java/com/example/q/parallaxtest/MainActivity.java
+++ app/src/main/java/com/example/q/parallaxtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ArrayAdapter;
@@ -11,25 +12,35 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private MyListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //加Header
-        final View mHeaderView=View.inflate(this,R.layout.view_header,null);
+        final View mHeaderView = View.inflate(this, R.layout.view_header, null);
         listView = (MyListView) findViewById(R.id.list_view);
-        final ImageView imageView= (ImageView) mHeaderView.findViewById(R.id.iv_header);
+        final ImageView imageView = (ImageView) mHeaderView.findViewById(R.id.iv_header);
         listView.addHeaderView(mHeaderView);
+        Log.e("onCreate", "还在onCreate中");
+        //onResume之后才可以得到
         mHeaderView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onGlobalLayout() {
-                //在布局填充结束构造完之后，此方法会被调用
+                //在布局填充结束构造完之后，所有的测绘工作都完成之后才会调用
+                Log.e("GlobalListener", "布局完成");
                 listView.setParallaxImage(imageView);//此时调用此方法便可以获得imageView的高度
                 mHeaderView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
         });
         //填充数据
-        listView.setAdapter(new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,Cheeses.names));
+        listView.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, Cheeses.names));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("TAG", "onResume");
     }
 }
--- app/src/main/java/com/example/q/parallaxtest/MyListView.java
+++ app/src/main/java/com/example/q/parallaxtest/MyListView.java
@@ -25,7 +25,6 @@ public class MyListView extends ListView {
     private ImageView imageView;
     private int mOriginalHeight;
     private int mDrawableHeight;
-    private IntEvaluator intEvaluator=new IntEvaluator();
 
     public MyListView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -47,50 +46,50 @@ public class MyListView extends ListView {
         //scrollRange：竖直方向最大范围
         //maxOverScrollY:竖直方向偏移最大量
         //isTouchEvent:是否是手指触摸滑动(true为手指，false为惯性)
+
         //手指拉动并且是最顶部下拉
         if (isTouchEvent && deltaY < 0) {
-            //把拉动的瞬时变化量交给Header，就可以只放大效果
-
-            if (imageView.getHeight()<= mDrawableHeight) {
-                imageView.getLayoutParams().height +=Math.abs(deltaY);
-                imageView.requestLayout();
+            //把拉动的瞬时变化量交给Header中的imageView，实现拉伸效果
+            if (imageView.getHeight() <= mDrawableHeight) {
+                imageView.getLayoutParams().height += Math.abs(deltaY);
+                imageView.requestLayout();//通知View，Layout已经修改
             }
         }
-        // Log.e("TAG", "overScrollBy: deltaY:" + deltaY + " scrollY:" + scrollY + " scrollRangeY:" + scrollRangeY + " maxOverScrollY" + maxOverScrollY + " isTouchEvent:" + isTouchEvent);
         return super.overScrollBy(deltaX, deltaY, scrollX, scrollY, scrollRangeX, scrollRangeY, maxOverScrollX, maxOverScrollY, isTouchEvent);
     }
 
     //设置图片，拿到引用
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public void setParallaxImage(ImageView imageView) {
         this.imageView = imageView;
-        //必须经过ListView测量才可以得到个高度,所以最好在onSizeChange拿到高度,这里的height和measuredHeight都等于0
-        mOriginalHeight=imageView.getHeight();
-        mDrawableHeight =  imageView.getDrawable().getIntrinsicHeight();
+        mOriginalHeight = imageView.getHeight();
+        mDrawableHeight = imageView.getDrawable().getIntrinsicHeight();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        switch (ev.getAction()){
+        switch (ev.getAction()) {
             case MotionEvent.ACTION_UP:
                 //执行回弹动画
                 //从当前高度到原始高度
-                final ValueAnimator mValueAnimator=ValueAnimator.ofInt(1);
-                mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                final ValueAnimator mValueAnimator = ValueAnimator.ofInt(imageView.getHeight(),mOriginalHeight);//从x->Y，但是并没有指定是哪个View
+                mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {//变化过程
                     @Override
                     public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                        float fragction=mValueAnimator.getAnimatedFraction();
-                        Log.e("TAG","fragction:"+fragction);//从0-1；
-                        int newHeight=intEvaluator.evaluate(fragction,imageView.getHeight(),mOriginalHeight);
-                        imageView.getLayoutParams().height=newHeight;
+                        imageView.getLayoutParams().height = (int) valueAnimator.getAnimatedValue();//得到x->y过程中的某个时刻的数值。然后自行修改height
                         imageView.requestLayout();
                     }
                 });
-                mValueAnimator.setInterpolator(new OvershootInterpolator());
+                mValueAnimator.setInterpolator(new OvershootInterpolator());//会弹效果,值越大弹力越大
                 mValueAnimator.setDuration(500);
                 mValueAnimator.start();
                 break;
         }
         return super.onTouchEvent(ev);
+}
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
     }
 }
